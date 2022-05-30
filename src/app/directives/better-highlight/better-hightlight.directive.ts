52d0f4efe840891e197ad71a9cbbbd6d302cd837
Jeremiah <jsnidar@gmail.com>
2022-05-29 19:05:01 -0600

    Set values in component

--- src/app/directives/better-highlight/better-hightlight.directive.ts
+++ src/app/directives/better-highlight/better-hightlight.directive.ts
@@ -1,31 +1,44 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef, 
+  HostBinding, 
+  HostListener, 
+  Input, 
+  OnInit, 
+  Renderer2 
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHightlight]'
 })
+
 export class BetterHightlightDirective implements OnInit {
+
+  @Input() defaultColor : string = 'transparent'
+  @Input() hightlightColor : string = 'blue'
 
   constructor(private renderer: Renderer2, private elRef: ElementRef) { }
 
   ngOnInit(){
     
   }
-  // HostBinding allows you to bind to a property of the dom element
-  @HostBinding ("style.backgroundColor") backgroundColor : string = "transparent"
+
+    // HostBinding allows you to bind to a property of the dom element
+  @HostBinding ("style.backgroundColor") backgroundColor : string = this.defaultColor
 
   @HostListener ('mouseenter') mouseover (eventData: Event) {
 
-    // You can use renderer methods to interact with the DOM
+      // You can use renderer methods to interact with the DOM
     // this.renderer.setStyle(this.elRef.nativeElement, "color", "blue")
 
-    //with HostBinding you can use this.propertyName to access that property,
-    //making it so you don't need to use renderer. 
-    //it looks nicer this way and uses less code
-    this.backgroundColor = 'blue'
+      //with HostBinding you can use this.propertyName to access that property,
+      //making it so you don't need to use renderer. 
+      //it looks nicer this way and uses less code
+    this.backgroundColor = this.hightlightColor
   }
 
   @HostListener ('mouseleave') mouseleave (eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, "color", "green")
-    this.backgroundColor = 'transparent'
+    this.backgroundColor = this.defaultColor
   }
 }
